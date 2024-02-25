4cca077237331391e12012a631b90266c3437213
Teixeiraass <guist2017@hotmail.com>
2024-02-25 13:45:04 -0300

    webconfig

--- src/main/java/com/notes/Notes/Project/config/WebConfig.java
+++ src/main/java/com/notes/Notes/Project/config/WebConfig.java
@@ -6,12 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/notes/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:10", "http://localhost:8080", "http://localhost:8081")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
